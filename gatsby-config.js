fa678488210672f71d74db886a05f55e87b3f294
Artem Rosnovsky <artem@rosnovsky.us>
2019-07-10 08:25:58 -0700

    Casper Init

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   siteMetadata: {
     title: `Rosnovsky Park™`,
@@ -8,275 +10,80 @@ module.exports = {
       twitter: `rosnovsky`,
       github: `rosnovsky`
     },
+    mapping: {
+      'MarkdownRemark.frontmatter.author': 'AuthorYaml',
+    },
   },
   plugins: [
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        path: `${__dirname}/content/`,
-        name: `blog`,
+        name: 'content',
+        path: path.join(__dirname, 'src', 'content'),
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/assets`,
-        name: `assets`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/podcast`,
-        name: `podcast`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
+      resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
           {
-            resolve: `gatsby-remark-images`,
+            resolve: 'gatsby-remark-responsive-iframe',
             options: {
-              maxWidth: 800,
+              wrapperStyle: 'margin-bottom: 1rem',
             },
           },
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
+          'gatsby-remark-abbr',
           {
-            resolve: 'gatsby-remark-embed-video',
+            resolve: 'gatsby-remark-images',
             options: {
-              width: 800,
-              ratio: 1.77, // Optional: Defaults to 16/9 = 1.77
-              related: false, //Optional: Will remove related videos from the end of an embedded YouTube video.
-              noIframeBorder: true, //Optional: Disable insertion of <style> border: 0
+              maxWidth: 1170,
+              quality: 90,
             },
           },
-          `gatsby-remark-reading-time`,
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
-            options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
-            },
-          },
-          `gatsby-remark-prismjs`,
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
         ],
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-json',
+    {
+      resolve: 'gatsby-plugin-canonical-urls',
+      options: {
+        siteUrl: 'https://gatsby-casper.netlify.com',
+      },
+    },
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-typescript',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-yaml',
+    'gatsby-plugin-feed',
+    {
+      resolve: 'gatsby-plugin-postcss',
+      options: {
+        postCssPlugins: [require('postcss-color-function'), require('cssnano')()],
+      },
+    },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: `UA-42452771-5`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-feed`,
-      options: {
-        query: `
-          {
-            site {
-              siteMetadata {
-                title
-                description
-                siteUrl
-                site_url: siteUrl
-              }
-            }
-          }
-        `,
-        setup: ({
-          query: {
-            site: { siteMetadata },
-            ...rest
-          },
-        }) => {
-          return {
-            ...siteMetadata,
-            ...rest,
-            description: "От автора Rosnovsky Park™ Weekly и Rosnovsky in Canada! Самый аутентичный подкаст на русском языке! Evergreen Podcast: Смотри ушами. Легендарный ведущий старейших подкастов на русском языке представляет третью серию подкастов — Вечнозелёный подкаст из вечнозелёного штата Вашингтон, что на Тихоокеанском Северо-Западе США 🇺🇸. Путешествия, походы, природа, работа, технологии, семья, дети, деньги — всё, что волнует интересует!",
-            custom_namespaces: {
-              'itunes': 'http://www.itunes.com/dtds/podcast-1.0.dtd',
-            },
-            custom_elements: [
-              {
-                'itunes:image': {
-                  _attr: {
-                    href: "https://podtema.com/wp-content/uploads/2018/07/cover.png"
-                  }
-                },
-
-                "itunes:owner":
-                {
-                  "itunes:name": "Artem Rosnovsky",
-                  "itunes:email": "artem@rosnovsky.us"
-                },
-
-                "itunes:category": "Personal Journals",
-                "itunes:category": "Places & Travel",
-                "itunes:category": "Society & Culture",
-
-                "itunes:keywords": "США, Канада, иммиграция, истории, Вашингтон, Орегон, росновский",
-                "itunes:explicit": "no",
-                "itunes:author": "Artem Rosnovsky",
-                "itunes:summary": "От автора Rosnovsky Park™ Weekly и Rosnovsky in Canada! Самый аутентичный подкаст на русском языке! Evergreen Podcast: Смотри ушами. Легендарный ведущий старейших подкастов на русском языке представляет третью серию подкастов — Вечнозелёный подкаст из вечнозелёного штата Вашингтон, что на Тихоокеанском Северо-Западе США 🇺🇸. Путешествия, походы, природа, работа, технологии, семья, дети, деньги — всё, что волнует интересует!",
-                "itunes:subtitle": "From Pacific Northwest to the World"
-              }
-            ]
-          }
-        },
-        feeds: [
-          {
-            serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.edges.map(edge => {
-                const siteUrl = site.siteMetadata.siteUrl
-                const postText = `
-                <div style="margin-top=55px; font-style: italic;">(This is a blog post I've posted at rosnovsky.us. You can read it <a href="${siteUrl +
-                  edge.node.fields.slug}">here</a>.)</div>
-              `
-
-                let html = edge.node.html
-                html = html
-                  .replace(/href="\//g, `href="${siteUrl}/`)
-                  .replace(/src="\//g, `src="${siteUrl}/`)
-                  .replace(/"\/static\//g, `"${siteUrl}/static/`)
-                  .replace(/,\s*\/static\//g, `,${siteUrl}/static/`)
-
-                return Object.assign({}, edge.node.frontmatter, {
-                  description: edge.node.frontmatter.spoiler,
-                  date: edge.node.fields.date,
-                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  custom_elements: [{ 'content:encoded': html + postText }],
-                })
-              })
-            },
-
-            query: `
-              {
-                allMarkdownRemark(
-                  limit: 1000,
-                  sort: { order: DESC, fields: [frontmatter___date] }
-                  filter: {frontmatter: { type: {ne: "podcast"}}}
-                ) {
-                  edges {
-                    node {
-                      excerpt(pruneLength: 250)
-                      html
-                      fields { 
-                        slug   
-                      }
-                      frontmatter {
-                        title
-                        date
-                        excerpt
-                      }
-                    }
-                  }
-                }
-              }
-            `,
-            output: '/rss.xml',
-            title: 'Rosnovsky Park™ RSS Feed',
-          },
-          {
-            serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.edges.map(edge => {
-                const siteUrl = site.siteMetadata.siteUrl
-                let html = edge.node.html
-                html = html
-                  .replace(/href="\//g, `href="${siteUrl}/`)
-                  .replace(/src="\//g, `src="${siteUrl}/`)
-                  .replace(/"\/static\//g, `"${siteUrl}/static/`)
-                  .replace(/,\s*\/static\//g, `,${siteUrl}/static/`)
-
-                return Object.assign({}, edge.node.frontmatter, {
-                  title: edge.node.frontmatter.title,
-                  description: edge.node.excerpt,
-                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  date: edge.node.frontmatter.date,
-                  lat: 47.84311, //optional latitude field for GeoRSS
-                  long: -122.204579, //optional longitude field for GeoRSS
-                  enclosure: { url: edge.node.frontmatter.source, size: edge.node.frontmatter.size * 1048576, type: "audio/mp3" },
-                  custom_elements: [
-                    {
-                      'itunes:duration': edge.node.frontmatter.time * 60,
-                      "content:encoded": html,
-                      "itunes:episode": edge.node.frontmatter.episode,
-                      "itunes:episodeType": edge.node.frontmatter.episodeType,
-                      "itunes:author": "Artem Rosnovsky",
-                    }
-                  ]
-                })
-              })
-            },
-
-            query: `
-            {
-              site {
-                siteMetadata {
-                  title
-                  description
-                  siteUrl
-                  site_url: siteUrl
-                }
-              }
-              allMarkdownRemark(
-                limit: 1000,
-                sort: { order: DESC, fields: [frontmatter___date] }
-                filter: {frontmatter: { type: {eq: "podcast"}}}
-                ) {
-                  edges {
-                    node {
-                      excerpt(pruneLength: 250)
-                      html
-                      fields { 
-                        slug   
-                      }
-                      frontmatter {
-                        title
-                        date(formatString: "MMMM DD, YYYY")
-                        lang
-                        type
-                        source
-                        cover {publicURL}
-                        time
-                        size
-                        episode
-                        episodeType
-                        mentions {type, text, url, isbn}
-                      }
-                    }
-                  }
-                }
-              }
-            `,
-            output: '/podcast.xml',
-            title: 'Evergreen Podcast',
-          },
-        ],
-      },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Rosnovsky Park™`,
-        short_name: `Rosnovsky.us`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `content/assets/favicon.ico`,
-      },
-    },
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
+        trackingId: 'UA-XXXX-Y',
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // IP anonymization for GDPR compliance
+        anonymize: true,
+        // Disable analytics for users with `Do Not Track` enabled
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ['/preview/**'],
+        // Specifies what percentage of users should be tracked
+        sampleRate: 100,
+        // Determines how often site speed tracking beacons will be sent
+        siteSpeedSampleRate: 10,
       },
     },
   ],
-}
+};
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,67 +1,179 @@
-const path = require(`path`)
-const { createFilePath } = require(`gatsby-source-filesystem`)
+const path = require('path');
+const _ = require('lodash');
 
-exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions;
 
-  const blogPost = path.resolve(`./src/templates/blog-post.js`)
-  const podcastPost = path.resolve(`./src/templates/podcast-post.js`)
-  return graphql(
-    `
-      {
-        allMarkdownRemark(
-          sort: { fields: [frontmatter___date], order: DESC }
-          limit: 1000
-        ) {
-          edges {
-            node {
-              fields {
-                slug
+  // Sometimes, optional fields tend to get not picked up by the GraphQL
+  // interpreter if not a single content uses it. Therefore, we're putting them
+  // through `createNodeField` so that the fields still exist and GraphQL won't
+  // trip up. An empty string is still required in replacement to `null`.
+  switch (node.internal.type) {
+    case 'MarkdownRemark': {
+      const { permalink, layout, primaryTag } = node.frontmatter;
+      const { relativePath } = getNode(node.parent);
+
+      let slug = permalink;
+
+      if (!slug) {
+        slug = `/${relativePath.replace('.md', '')}/`;
+      }
+
+      // Used to generate URL to view this content.
+      createNodeField({
+        node,
+        name: 'slug',
+        value: slug || '',
+      });
+
+      // Used to determine a page layout.
+      createNodeField({
+        node,
+        name: 'layout',
+        value: layout || '',
+      });
+
+      createNodeField({
+        node,
+        name: 'primaryTag',
+        value: primaryTag || '',
+      });
+    }
+  }
+};
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+
+  const result = await graphql(`
+    {
+      allMarkdownRemark(
+        limit: 2000
+        sort: { fields: [frontmatter___date], order: ASC }
+        filter: { frontmatter: { draft: { ne: true } } }
+      ) {
+        edges {
+          node {
+            excerpt
+            timeToRead
+            frontmatter {
+              title
+              tags
+              date
+              draft
+              image {
+                childImageSharp {
+                  fluid(maxWidth: 3720) {
+                    aspectRatio
+                    base64
+                    sizes
+                    src
+                    srcSet
+                  }
+                }
               }
-              frontmatter {
-                title
-                lang
-                type
+              author {
+                id
+                bio
+                avatar {
+                  children {
+                    ... on ImageSharp {
+                      fixed(quality: 90) {
+                        src
+                      }
+                    }
+                  }
+                }
               }
+            }
+            fields {
+              layout
+              slug
             }
           }
         }
       }
-    `
-  ).then(result => {
-    if (result.errors) {
-      throw result.errors
+      allAuthorYaml {
+        edges {
+          node {
+            id
+          }
+        }
+      }
     }
+  `);
 
-    // Create blog posts pages.
-    const posts = result.data.allMarkdownRemark.edges
-
-    posts.forEach((post, index) => {
-      const previous = index === posts.length - 1 ? null : posts[index + 1].node
-      const next = index === 0 ? null : posts[index - 1].node
-
-      createPage({
-        path: post.node.fields.slug,
-        component: post.node.frontmatter.type == "podcast" ? podcastPost : blogPost,
-        context: {
-          slug: post.node.fields.slug,
-          previous,
-          next,
-        },
-      })
-    })
-  })
-}
-
-exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
-
-  if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
-    createNodeField({
-      name: `slug`,
-      node,
-      value,
-    })
+  if (result.errors) {
+    console.error(result.errors);
+    throw new Error(result.errors);
   }
-}
+
+  // Create post pages
+  const posts = result.data.allMarkdownRemark.edges;
+  posts.forEach(({ node }, index) => {
+    const { slug, layout } = node.fields;
+    const prev = index === 0 ? null : posts[index - 1].node;
+    const next = index === posts.length - 1 ? null : posts[index + 1].node;
+
+    createPage({
+      path: slug,
+      // This will automatically resolve the template to a corresponding
+      // `layout` frontmatter in the Markdown.
+      //
+      // Feel free to set any `layout` as you'd like in the frontmatter, as
+      // long as the corresponding template file exists in src/templates.
+      // If no template is set, it will fall back to the default `post`
+      // template.
+      //
+      // Note that the template has to exist first, or else the build will fail.
+      component: path.resolve(`./src/templates/${layout || 'post'}.tsx`),
+      context: {
+        // Data passed to context is available in page queries as GraphQL variables.
+        slug,
+        prev,
+        next,
+        primaryTag: node.frontmatter.tags ? node.frontmatter.tags[0] : '',
+      },
+    });
+  });
+
+  // Create tag pages
+  const tagTemplate = path.resolve('./src/templates/tags.tsx');
+  const tags = _.uniq(
+    _.flatten(
+      result.data.allMarkdownRemark.edges.map(edge => {
+        return _.castArray(_.get(edge, 'node.frontmatter.tags', []));
+      }),
+    ),
+  );
+  tags.forEach(tag => {
+    createPage({
+      path: `/tags/${_.kebabCase(tag)}/`,
+      component: tagTemplate,
+      context: {
+        tag,
+      },
+    });
+  });
+
+  // Create author pages
+  const authorTemplate = path.resolve('./src/templates/author.tsx');
+  result.data.allAuthorYaml.edges.forEach(edge => {
+    createPage({
+      path: `/author/${_.kebabCase(edge.node.id)}/`,
+      component: authorTemplate,
+      context: {
+        author: edge.node.id,
+      },
+    });
+  });
+};
+
+exports.onCreateWebpackConfig = ({ stage, actions }) => {
+  // adds sourcemaps for tsx in dev mode
+  if (stage === `develop` || stage === `develop-html`) {
+    actions.setWebpackConfig({
+      devtool: 'eval-source-map',
+    });
+  }
+};
