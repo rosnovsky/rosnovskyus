b744d9d3f59be69d1f694b1ebb909afb6010627c
Artem Rosnovsky <inbox@podtema.com>
2019-07-15 20:14:21 -0700

    fixed config

--- gatsby-config.js
+++ gatsby-config.js
@@ -98,7 +98,6 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/favicon.ico`,
       },
     },
     `gatsby-plugin-offline`,
